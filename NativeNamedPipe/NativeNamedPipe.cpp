a97a0bf5b70c9f80d56238d972fe6b2c3a7388a1
Lachee <lachlan.h@hotmail.com>
2018-10-15 13:52:44 +1100

    Updated main

--- NativeNamedPipe/NativeNamedPipe.cpp
+++ NativeNamedPipe/NativeNamedPipe.cpp
@@ -3,22 +3,24 @@
 
 #include "NativeNamedPipe.h"
 
-extern "C" NATIVEPIPE_API NamedPipeClient* CreateNamedPipeClient()
-{
-	return new NamedPipeClient();
-}
+extern "C" NATIVEPIPE_API BaseNamedPipeClient* CreateClient() { return BaseNamedPipeClient::create(); }
 
-extern "C" NATIVEPIPE_API void DestroyNamedPipeClient(NamedPipeClient* client) 
+extern "C" NATIVEPIPE_API void DestroyClient(BaseNamedPipeClient* client)
 {
 	if (client != NULL) 
 	{
+		//close the client
+		if (client->isConnected())
+			client->close();
+
+		//delete and nullify the client
 		delete client;
 		client = NULL;
 	}
 }
 
-extern "C" NATIVEPIPE_API bool isConnected(NamedPipeClient* client) { return client->isConnected(); }
-extern "C" NATIVEPIPE_API int readFrame(NamedPipeClient* client, unsigned char* buffer, int length) { return client->readFrame(buffer, length); }
-extern "C" NATIVEPIPE_API int writeFrame(NamedPipeClient* client, unsigned char* buffer, int length) { return client->writeFrame(buffer, length); }
-extern "C" NATIVEPIPE_API int open(NamedPipeClient* client, char* pipename) { return client->open(pipename); }
-extern "C" NATIVEPIPE_API void close(NamedPipeClient* client) { client->close(); }
+extern "C" NATIVEPIPE_API bool isConnected(BaseNamedPipeClient* client) { return client->isConnected(); }
+extern "C" NATIVEPIPE_API int readFrame(BaseNamedPipeClient* client, unsigned char* buffer, int length) { return client->readFrame(buffer, length); }
+extern "C" NATIVEPIPE_API int writeFrame(BaseNamedPipeClient* client, unsigned char* buffer, int length) { return client->writeFrame(buffer, length); }
+extern "C" NATIVEPIPE_API int open(BaseNamedPipeClient* client, char* pipename) { return client->open(pipename); }
+extern "C" NATIVEPIPE_API void close(BaseNamedPipeClient* client) { client->close(); }
--- NativeNamedPipe/connection.h
+++ NativeNamedPipe/connection.h
@@ -1,8 +1,8 @@
 class BaseNamedPipeClient
 {
+public:
 	static BaseNamedPipeClient* create();
 
-public: 
 	bool isConnected();
 	int readFrame(unsigned char* buffer, int length);
 	int writeFrame(unsigned char* buffer, int length);
