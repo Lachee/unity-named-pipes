3774618a436abadeb979d4731a7e39b65a5ff1d8
Lachee <lachlan.h@hotmail.com>
2018-10-15 13:57:24 +1100

    Updated Unix

--- NativeNamedPipe/connection_unix.cpp
+++ NativeNamedPipe/connection_unix.cpp
@@ -15,38 +15,65 @@ static int MsgFlags = MSG_NOSIGNAL;
 static int MsgFlags = 0;
 #endif
 
-
-NamedPipeClient::NamedPipeClient()
+class BaseNamedPipeClient
 {
-	handle = NULL;
+	static BaseNamedPipeClient* BaseNamedPipeClient::create() { return (BaseNamedPipeClient*)(new NamedPipeClientUnix()); }
+};
+
+class NamedPipeClientUnix : BaseNamedPipeClient
+{
+public:
+	NamedPipeClientUnix()
+	{
+		socket = -1;
+		isOpened = false;
+	}
+
+	bool isConnected();
+	int readFrame(unsigned char* buffer, int length);
+	int writeFrame(unsigned char* buffer, int length);
+
+	int open(char* pipename);
+	void close(void);
+
+private:
+	int socket;
+	bool isOpened;
+};
+
+
+
+NamedPipeClientUnix::NamedPipeClient()
+{
+	socket = -1;
 	isOpened = false;
 }
 
-bool NamedPipeClient::isConnected()
+bool NamedPipeClientUnix::isConnected()
 {
-	return isOpened && handle != NULL && handle != -1;
+	return isOpened && socket != -1;
 }
 
-int NamedPipeClient::open(char* pipename)
+int NamedPipeClientUnix::open(char* pipename)
 {
 	sockaddr_un addr;
 	addr.sun_family = AF_UNIX;
 
 	//Create the socket
-	handle = socket(AF_UNIX, SOCK_STREAM, 0);
-	if (handle == -1) return -1;
+	socket = socket(AF_UNIX, SOCK_STREAM, 0);
+	if (socket == -1) return -1;
 
 	//Yes.. do what this does
-	fcntl(handle, F_SETFL, O_NONBLOCK);
+	fcntl(socket, F_SETFL, O_NONBLOCK);
 
 #ifdef SO_NOSIGPIPE
 	int optval = 1;
-	setsockopt(handle, SOL_SOCKET, SO_NOSIGPIPE, &optval, sizeof(optval));
+	setsockopt(socket, SOL_SOCKET, SO_NOSIGPIPE, &optval, sizeof(optval));
 #endif
 
 	//Update the address
 	snprintf(addr.sun_path, sizeof(addr.sun_path), "%s", pipename);
-	int err = connect(handle, (const sockaddr*)&addr, sizeof(addr));
+	int err = connect(socket, (const sockaddr*)&addr, sizeof(addr));
 	if (err == 0) {
 		isOpened = true;
 		return 0;
@@ -56,34 +83,32 @@ int NamedPipeClient::open(char* pipename)
 	return err;
 }
 
-void NamedPipeClient::close()
+void NamedPipeClientUnix::close()
 {
-	if (handle != NULL && handle != -1)
-		close(handle);
-
-	handle = -1;
+	if (isConnected()) close(socket);
+	socket = -1;
 	isOpened = false;
 }
 
-int NamedPipeClient::readFrame(unsigned char* buffer, int length)
+int NamedPipeClientUnix::readFrame(unsigned char* buffer, int length)
 {
 	if (length == 0) return 0;
 	if (!isConnected()) return -1;
 
 	size_t bytesLength = (size_t)length;
-	int res = (int)recv(handle, data, bytesLength, MsgFlags);
+	int res = (int)recv(socket, data, bytesLength, MsgFlags);
 	if (res < 0) return (int)res - 1;
 
 	return res;
 }
 
-int NamedPipeClient::writeFrame(unsigned char* buffer, int length)
+int NamedPipeClientUnix::writeFrame(unsigned char* buffer, int length)
 {
 	if (length == 0) return 0;
 	if (!isConnected()) return -1;
 
 	size_t bytesLength = (size_t)length;
-	ssize_t sentBytes = send(handle, buffer, length, MsgFlags);
+	ssize_t sentBytes = send(socket, buffer, length, MsgFlags);
 	if (sentBytes < 0) return (int)sentBytes - 1;
 	return (int)sentBytes;
 }
