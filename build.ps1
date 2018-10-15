83162cdd3e75c90e31521b0b61f4cb04020ea8bf
Lachee <lachlan.h@hotmail.com>
2018-10-16 10:38:37 +1100

    Improved build script and removed test scene

--- build.ps1
+++ build.ps1
@@ -1,7 +1,8 @@
 ﻿#powershell -ExecutionPolicy ByPass -File build.ps1
 [CmdletBinding()]
 Param(
-	[switch]$BuildLibrary
+	[switch]$BuildLibrary,
+	[switch]$BuildPackage
 )
 
 function BuildLibrary([string] $target) 
@@ -29,11 +30,33 @@ function CollectArtifacts([string] $target)
 	cp "build\\NativeNamedPipe\\$target\\NativeNamedPipe.dll" "artifacts\\NativeNamedPipe.dll"
 }
 
+function RunUnityExporter([string] $install_dir, [string]  $source, [string] $output) 
+{    
+    Write-Output "Installing Discord Unity Exporter...";
+    mkdir -Force "$install_dir";
+
+    git clone https://github.com/Lachee/Unity-Package-Exporter.git "$install_dir"  2>&1 | % { $_.ToString() }
+    dotnet build "$install_dir\\UnityPackageExporter";
+
+    Write-Output "Exporting...";
+    Write-Output "$install_dir";
+    Write-Output "$source";
+    Write-Output "$output";
+    dotnet run --project "$install_dir\\UnityPackageExporter" -project "$source" -output "$output" -a
+}
+
+
 if ($BuildLibrary)
 {
 	Write-Host "Building Library"
     BuildLibrary "Release"
 	CollectArtifacts "Release"
+}
+
+if ($BuildPackage) 
+{
+	Write-Host "Building Unity Package..."
+	RunUnityExporter "tools/unity-package-exporter" "UnityNamedPipes" "artifacts/UnityNamedPipes.unitypackage"
 }
 
 Write-Host "DONE"
