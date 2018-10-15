1c022954ae9447a082c3eb016b04791fffeab4cd
Lachee <lachlan.h@hotmail.com>
2018-10-15 17:06:37 +1100

    added a buildscript

---
+++ build.ps1
@@ -0,0 +1,9 @@
+﻿#powershell -ExecutionPolicy ByPass -File build.ps1
+
+Write-Host "Generating CMake..."
+mkdir build --Forced
+cd build
+cmake ..
+
+Write-Host "Building Project..."
+msbuild NativeNamedPipe
