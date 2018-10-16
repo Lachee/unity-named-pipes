547d25d58f449e248dc02222749d848a5b33becd
Lachee <lachlan.h@hotmail.com>
2018-10-16 12:08:57 +1100

    more buildscript fixes

--- build-package.ps1
+++ build-package.ps1
@@ -1,20 +1,20 @@
 ﻿function RunUnityExporter([string] $install_dir, [string]  $source, [string] $output) 
 {    
     Write-Output "Installing Discord Unity Exporter...";
-    mkdir -Force "$install_dir";
+    mkdir "$install_dir";
 
     git clone https://github.com/Lachee/Unity-Package-Exporter.git "$install_dir"  2>&1 | % { $_.ToString() }
-    dotnet build "$install_dir\\UnityPackageExporter";
+    dotnet build "$install_dir/UnityPackageExporter";
 
-    Write-Output "Exporting...";
-    Write-Output "$install_dir";
-    Write-Output "$source";
-    Write-Output "$output";
-    dotnet run --project "$install_dir\\UnityPackageExporter" -project "$source" -output "$output" -a
+    Write-Output "Exporting Unity Package";
+    Write-Output "Install:	$install_dir";
+    Write-Output "Source:	$source";
+    Write-Output "Output:	$output";
+    dotnet run --project "$install_dir/UnityPackageExporter" -project "$source" -output "$output" -a
 }
 
 Write-Host "=== Unity Named Pipe Unity Build ==="
-mkdir "artifacts" -Force
+mkdir "artifacts"
 RunUnityExporter "tools/unity-package-exporter" "UnityNamedPipes" "artifacts/UnityNamedPipes.unitypackage"
 
 Write-Host "Done!"
