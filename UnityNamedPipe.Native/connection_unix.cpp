6a2efa53d2295ca676707bc9606af9c92e9280fd
Lachee <lachlan.h@hotmail.com>
2018-10-20 15:16:49 +1100

    xn_getlasterror

--- UnityNamedPipe.Native/connection_unix.cpp
+++ UnityNamedPipe.Native/connection_unix.cpp
@@ -36,7 +36,7 @@ public:
 
 		size_t bytesLength = (size_t)length;
 		int res = (int)recv(sock, buffer, bytesLength, MsgFlags);
-		if (res == UDT::ERROR) return -1 * (::getlasterror().getErrorCode());
+		if (res < 0) return -1 * (xn_getlasterror());
 
 		return res;
 	}
