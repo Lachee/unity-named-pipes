99873a1f5294387d291c1c6030093d0eb7f55ca5
Lachee <lachlan.h@hotmail.com>
2019-03-17 11:47:02 +1100

    seperating 64bitness

--- build-library.ps1
+++ build-library.ps1
@@ -1,12 +1,11 @@
 ﻿#powershell -ExecutionPolicy ByPass -File build-library.ps1
 
 param(
-	[string] $target,
-	[switch] $64bit
+	[switch] $make64bit
 )
 
 #https://gist.github.com/IlyaFinkelshteyn/79af78657660e118b15d3ab9d62ab8a1
-function BuildLibrary([string] $target, [bool] $is64bit) 
+function BuildLibrary([bool] $is64bit) 
 {
     Write-Host "Generating CMake..."
     mkdir build -Force
@@ -17,7 +16,7 @@ function BuildLibrary([string] $target, [bool] $is64bit)
 		Write-Host "Generating 64bit..."
     	cmake .. -DCMAKE_GENERATOR_PLATFORM=x64
         
-		Write-Host "Generating Build Target {$target} ..."
+		Write-Host "Generating Build Target..."
 		#msbuild "NativeNamedPipe.sln" /p:Configuration=$target /p:Platform=x64
 		msbuild "NativeNamedPipe.sln" /p:Configuration=Release /p:Platform=x64
 	}
@@ -26,7 +25,7 @@ function BuildLibrary([string] $target, [bool] $is64bit)
 		Write-Host "Generating 32bit..."
     	cmake ..
         
-		Write-Host "Generating Build Target {$target} ..."
+		Write-Host "Generating Build Target..."
 		#msbuild "NativeNamedPipe.sln" /p:Configuration=$target
 		msbuild "NativeNamedPipe.sln" /p:Configuration=Release
 	}
@@ -57,13 +56,13 @@ function BuildLibrary([string] $target, [bool] $is64bit)
 }
 
 Write-Host "=== Unity Named Pipe Windows Build ==="
-if ($64bit) 
+if ($make64bit) 
 {
-	BuildLibrary $target $true
+	BuildLibrary $true
 }
 else
 {
-	BuildLibrary $target $false
+	BuildLibrary $false
 }
 
 Write-Host "Done!"
